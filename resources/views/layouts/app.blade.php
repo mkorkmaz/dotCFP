ba6caf9b18546a5f8ae8abf53a22e35b07c0fefa
Emir Karsiyakali <emirkarsiyakali@gmail.com>
2017-11-20 08:04:40 +0300

    changed dates and added analytics

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -139,6 +139,15 @@
 <!-- Scripts -->
 <script src="{{ asset('js/app.js') }}"></script>
 <script src="{{ asset('js/bootbox.js') }}"></script>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-109911034-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-109911034-1');
+</script>
 
 </body>
 </html>
